58c5be3fcb329a153bc6414be9623d2fd76b8207
Caio Pratali <caio@pratali.com.br>
2021-07-03 23:36:59 -0300

    config ts/eslint/prettier

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.status(200).json({ ok: true })
-})
+app.get("/", (req, res) => {
+  return res.status(200).json({ ok: true });
+});
 
-app.listen(3333, () => "Server ON");
+app.listen(3333, () => console.log("Server is running!"));
